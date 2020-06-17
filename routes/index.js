71b2f111eb554cc7f7180b66dadd996495986e87
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-06-17 23:59:49 +0530

    Changed to give url

--- routes/index.js
+++ routes/index.js
@@ -13,11 +13,13 @@ const checkURL = function (item) {
   const caption = item.caption;
   caption.split(" ").forEach((part) => {
     const regexWithoutHttp = /[-a-zA-Z0-9@:%.+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%+.~#?&//=]*)/;
-    const regexWithHttp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%.+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%+.~#?&//=]*)/;
+    // const regexWithHttp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%.+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%+.~#?&//=]*)/;
 
-    if (part.match(regexWithoutHttp) || part.match(regexWithHttp)) {
+    if (part.match(regexWithoutHttp)) {
       part = part.split(/[\r\n]+/gm);
       item.url = part[0];
+    } else{
+      item.url = ""
     }
   });
 };
