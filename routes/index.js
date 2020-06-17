55d07c9232c189da499b8900474950909025c06e
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-06-18 00:16:38 +0530

    Added Unit Test

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,31 @@
-const router = require("express").Router();
-const axios = require("axios");
-const redis = require("redis");
-const dotEnv = require("dotenv");
+const router = require('express').Router();
+const axios = require('axios');
+const redis = require('redis');
+const dotEnv = require('dotenv');
 dotEnv.config();
 const client = redis.createClient(process.env.REDIS_URL);
-client.on("error", function (error) {
+client.on('error', function (error) {
   console.error(error);
 });
 
 const url = `https://graph.facebook.com/${process.env.INSTAGRAM_ID}/media?fields=id,media_type,media_url,timestamp,caption&access_token=${process.env.ACCESS_TOKEN}`;
 const checkURL = function (item) {
   const caption = item.caption;
-  caption.split(" ").forEach((part) => {
+  caption.split(' ').forEach((part) => {
     const regexWithoutHttp = /[-a-zA-Z0-9@:%.+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%+.~#?&//=]*)/;
     // const regexWithHttp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%.+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%+.~#?&//=]*)/;
 
     if (part.match(regexWithoutHttp)) {
       part = part.split(/[\r\n]+/gm);
       item.url = part[0];
-    } else{
-      item.url = ""
+    } else {
+      item.url = '';
     }
   });
 };
 
-router.get("/", (req, res) => {
-  client.get("completeData", async (err, reply) => {
+router.get('/', (req, res) => {
+  client.get('completeData', async (err, reply) => {
     if (err) {
       return res.send(err);
     }
@@ -38,20 +38,20 @@ router.get("/", (req, res) => {
         const data = result.data.data;
         data.forEach(checkURL);
         client.set(
-          "completeData",
+          'completeData',
           JSON.stringify({
-            data: data,
+            data: data
           }),
-          "EX",
+          'EX',
           7 * 60 * 60
         );
         return res.status(200).json({
-          data: data,
+          data: data
         });
       } catch (err) {
         console.log(err);
         res.status(500).json({
-          error: err,
+          error: err
         });
       }
     }
